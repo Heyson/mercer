745b116006bf299528e7b8b1272654b7b0070e10
Heyson <hs@heysonseverino.com>
2021-12-19 12:34:21 -0500

    Add files via upload

---
+++ bosch-newhire/tipuesearch/tipuesearch_set.js
@@ -0,0 +1,62 @@
+
+
+/*
+
+Tipue Search 2.1
+
+Copyright (c) 2013 Tipue
+
+Tipue Search is released under the MIT License
+
+http://www.tipue.com/search
+
+*/
+
+
+
+
+
+var tipuesearch_stop_words = ["and", "be", "by", "do", "for", "he", "how", "if", "is", "it", "my", "not", "of", "or", "the", "to", "up", "what", "when"];
+
+
+
+var tipuesearch_replace = {"words": [
+
+     {"word": "tipua", replace_with: "tipue"},
+
+     {"word": "javscript", replace_with: "javascript"}
+
+]};
+
+
+
+var tipuesearch_stem = {"words": [
+
+     {"word": "e-mail", stem: "email"},
+
+     {"word": "javascript", stem: "script"},
+
+     {"word": "javascript", stem: "js"}
+
+]};
+
+
+
+var tipuesearch_pages = ["http://www.mercerhrs.com/microsite/bosch/rbna/index.shtml",
+ "http://www.mercerhrs.com/microsite/bosch/rbna/new.shtml",
+ "http://www.mercerhrs.com/microsite/bosch/rbna/health_overview.shtml",
+  "http://www.mercerhrs.com/microsite/bosch/rbna/medical.shtml",
+   "http://www.mercerhrs.com/microsite/bosch/rbna/dental.shtml",
+    "http://www.mercerhrs.com/microsite/bosch/rbna/vision.shtml", 
+	"http://www.mercerhrs.com/microsite/bosch/rbna/fsa_hsa.shtml",
+	 "http://www.mercerhrs.com/microsite/bosch/rbna/wellness.shtml", 
+	 "http://www.mercerhrs.com/microsite/bosch/rbna/be_well.shtml", 
+	 "http://www.mercerhrs.com/microsite/bosch/rbna/other_benefits.shtml", 
+	 "http://www.mercerhrs.com/microsite/bosch/rbna/enroll.shtml",
+	  "http://www.mercerhrs.com/microsite/bosch/rbna/tools.shtml",
+	   "http://www.mercerhrs.com/microsite/bosch/rbna/contacts.shtml",
+	   "http://www.mercerhrs.com/microsite/bosch/rbna/legal.shtml"
+	   ];
+
+
+
